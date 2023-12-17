5ea0b35fef6ea51c8f6f22a43481f423259bc0d5
perwriters <perwriters@gmail.com>
2023-12-17 10:38:13 +0300

    Finished the HorizontalScrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { data } from "./MockData";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import "./index.css";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
+  return (
+    <>
+      <img
+        className="cover-image"
+        src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2607&q=80"
+        alt=""
+      />
+      <div className="slider-container">
+        <MdChevronLeft className="chevron-icon" onClick={slideLeft} size={40} />
+        <div id="slider" className="slider-container">
+          {data.map((item, index) => (
+            <img
+              key={index}
+              className="slider-image"
+              src={item.img}
+              alt={`Slide ${index + 1}`}
+            />
+          ))}
+        </div>
+        <MdChevronRight
+          className="chevron-icon"
+          onClick={slideRight}
+          size={40}
+        />
+      </div>
+    </>
+  );
+}
 
 export default App;
